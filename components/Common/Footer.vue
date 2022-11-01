98240c7965313f62e9123fc89d7d30c3b8135fd4
min <minyore@naver.com>
2022-11-01 14:30:00 +0900

    Header Footer 작업

---
+++ components/Common/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <div class="innerSection">
+      <p>{{ copyRight }}</p>
+      <p>{{ officer }}</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {
+      copyRight: "© 2022 ANNOTATION AI. All RIGHTS RESERVED.",
+      officer: "DPO(Data Protection Officer) : DongKuk Yoon",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/common.scss";
+</style>
