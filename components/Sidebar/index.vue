f3e7974cf3410cd97923e4492c9d566dc8ebb2d6
min <minyore@naver.com>
2022-11-08 11:24:08 +0900

    Left Menu css 수정 및 코드 정리

--- components/Sidebar/index.vue
+++ components/Sidebar/index.vue
@@ -1,118 +1,4 @@
 <template>
-  <!-- <div :class="{ 'has-logo': showLogo }">
-    <logo :collapse="isCollapse" />
-    <el-scrollbar wrap-class="scrollbar-wrapper">
-      <el-menu
-        :collapse="isCollapse"
-        :background-color="variables.menuBg"
-        :text-color="variables.menuText"
-        :unique-opened="false"
-        :active-text-color="variables.menuActiveText"
-        :collapse-transition="false"
-        mode="vertical"
-      >
-        <el-menu-item index="0">
-          <span slot="title">
-            <NuxtLink
-              to="/table"
-              style="
-                font-size: 1.5rem;
-                font-family: Avenir, Helvetica Neue, Arial, Helvetica,
-                  sans-serif;
-              "
-              >table</NuxtLink
-            >
-          </span>
-        </el-menu-item>
-        <el-menu-item index="1">
-          <span slot="title">
-            <NuxtLink
-              to="/form"
-              style="
-                font-size: 1.5rem;
-                font-family: Avenir, Helvetica Neue, Arial, Helvetica,
-                  sans-serif;
-              "
-              >form</NuxtLink
-            >
-          </span>
-        </el-menu-item>
-        <el-menu-item index="2">
-          <span slot="title">
-            <NuxtLink
-              to="/Project/ProjectList"
-              style="
-                font-size: 1.5rem;
-                font-family: Avenir, Helvetica Neue, Arial, Helvetica,
-                  sans-serif;
-              "
-              >Project</NuxtLink
-            >
-          </span>
-        </el-menu-item>
-       <el-menu-item index="1">
-          <span slot="title">
-            <NuxtLink
-              to="/imageTask"
-              style="
-                font-size: 1.5rem;
-                font-family: Avenir, Helvetica Neue, Arial, Helvetica,
-                  sans-serif;
-              "
-              >imageTask</NuxtLink
-            >
-          </span>
-        </el-menu-item> -->
-
-  <!-- <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>multi</span>
-          </template>
-          <el-menu-item-group>
-            <template slot="title">1</template>
-            <el-menu-item index="1-1">1</el-menu-item>
-            <el-menu-item index="1-2">2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="2">
-            <el-menu-item index="1-3">1</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">4</template>
-            <el-menu-item index="1-4-1">4</el-menu-item>
-          </el-submenu>
-        </el-submenu> -->
-  <!-- <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
-          <span slot="title">
-            <NuxtLink
-              to="/member/member"
-              style="
-                font-size: 1.5rem;
-                font-family: Avenir, Helvetica Neue, Arial, Helvetica,
-                  sans-serif;
-              "
-              >member</NuxtLink
-            >
-          </span>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <i class="el-icon-menu"></i>
-          <span slot="title">
-            <NuxtLink
-              to="/taskLable"
-              style="
-                font-size: 1.5rem;
-                font-family: Avenir, Helvetica Neue, Arial, Helvetica,
-                  sans-serif;
-              "
-              >taskLable</NuxtLink
-            >
-          </span>
-        </el-menu-item> 
-      </el-menu>
-    </el-scrollbar>
-  </div> -->
   <div class="leftMenu">
     <el-scrollbar wrap-class="scrollbar-wrapper">
       <!-- <el-radio-group v-model="isCollapse">
@@ -145,6 +31,7 @@
             </el-menu-item>
           </el-menu-item-group>
         </el-submenu>
+
         <el-submenu index="2">
           <template slot="title">
             <span class="svg-container">
@@ -161,6 +48,7 @@
             </el-menu-item>
           </el-menu-item-group>
         </el-submenu>
+
         <el-submenu index="3">
           <template slot="title">
             <span class="svg-container">
@@ -217,19 +105,16 @@
 
 <script>
 import { mapGetters } from "vuex";
-//import Logo from "./Logo";
-//import variables from "@/assets/styles/variables.scss";
-import variables from "@/assets/styles/common.scss";
+import variables from "@/assets/styles/variables.scss";
 
 export default {
-  // components: { Logo },
   computed: {
     variables() {
       return variables;
     },
-    isCollapse() {
-      return false;
-    },
+    // isCollapse() {
+    //   return false;
+    // },
   },
   data() {
     return {
@@ -248,128 +133,5 @@ export default {
 </script>
 
 <style lang="scss">
-.leftMenu {
-  position: relative;
-  max-width: 240px;
-  width: 100%;
-  height: 100%;
-  background-color: #20212b;
-
-  &:not(.el-menu--collapse) {
-    max-width: 240px;
-    width: 100%;
-  }
-
-  .el-menu {
-    padding: 8px 24px;
-    display: flex;
-    flex-direction: column;
-    gap: 16px 0;
-    background-color: #20212b;
-
-    .el-submenu,
-    .el-menu-item {
-      padding: 0;
-      color: #fff;
-      font-size: 16px;
-      font-weight: 500;
-
-      &:focus,
-      &:hover,
-      &:active {
-        background-color: transparent;
-      }
-
-      &.is-opened {
-        .el-submenu__title,
-        .el-submenu__icon-arrow {
-          color: #168fff !important;
-        }
-
-        & .svg-icon {
-          fill: #168fff !important;
-        }
-      }
-
-      // Title
-      .el-submenu__title {
-        padding: 0 12px !important;
-        display: flex;
-        align-items: center;
-        gap: 0 16px;
-        width: 100%;
-        height: 48px;
-        line-height: 24px;
-        color: #fff !important;
-        font-size: 16px;
-        font-weight: 500;
-
-        &:hover {
-          background-color: transparent !important;
-
-          span,
-          .el-submenu__icon-arrow {
-            color: #168fff !important;
-          }
-
-          & .svg-icon {
-            fill: #168fff !important;
-          }
-        }
-
-        span {
-          width: calc(100% - 60px);
-        }
-
-        // Icon
-        & .svg-container {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          width: 24px;
-          height: 24px;
-
-          .svg-icon {
-            width: 24px;
-            height: 24px;
-          }
-        }
-
-        .el-submenu__icon-arrow {
-          right: 12px;
-          width: 16px;
-          height: 16px;
-          color: #fff;
-          font-size: 16px;
-          font-weight: bold;
-        }
-      }
-    }
-
-    // Depth
-    .el-menu--inline {
-      padding: 0;
-      gap: 0;
-
-      .el-menu-item-group {
-        .el-menu-item-group__title {
-          display: none;
-        }
-
-        ul {
-          display: flex;
-          flex-direction: column;
-          gap: 8px;
-        }
-
-        .el-menu-item {
-          min-width: 100% !important;
-          height: 100%;
-          font-size: 14px;
-          line-height: 21px;
-        }
-      }
-    }
-  }
-}
+@import "@/assets/styles/sidebar.scss";
 </style>
